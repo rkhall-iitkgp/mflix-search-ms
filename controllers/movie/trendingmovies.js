963895aec4eff748f6b34117f96b23d3a024c934
Girish Lodha <girish.lodha08@gmail.com>
2024-03-20 22:05:30 +0530

    .

--- controllers/movie/trendingmovies.js
+++ controllers/movie/trendingmovies.js
@@ -4,16 +4,17 @@ async function TrendingMovies(req, res) {
     try {
         const allMovies = await Movie.find();
 
-        const ratedMovies = allMovies.filter(movie => movie.imdb && movie.tomatoes && movie.released);
+        const ratedMovies = allMovies.filter(movie => movie.tomatoes.viewer.meter && movie.year);
 
         // Calculate weighted score for each movie
         ratedMovies.forEach(movie => {
             const tomatoesMeter = movie.tomatoes.viewer.meter;
-            const releaseDate = new Date(movie.released);
+            const releaseYear = movie.year;
             const currentDate = new Date();
+            const currentYear = currentDate.getFullYear();
 
             // Weight for release date (earlier is better)
-            const releaseWeight = 1 / (currentDate - releaseDate);
+            const releaseWeight = 1 / (currentYear + 1 - releaseYear);
             // Calculate weighted score
             movie.weightedScore = releaseWeight * (tomatoesMeter / 100);
         });
@@ -22,6 +23,7 @@ async function TrendingMovies(req, res) {
         ratedMovies.sort((a, b) => b.weightedScore - a.weightedScore);
 
         const topTrendingMovies = ratedMovies.slice(0, 10);
+
 
         res.json(topTrendingMovies);
     }
