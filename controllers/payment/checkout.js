d59161a50956a518324a50487e19f3aa55c64f67
Girish Lodha <girish.lodha08@gmail.com>
2024-03-21 01:53:31 +0530

    .

--- controllers/payment/checkout.js
+++ controllers/payment/checkout.js
@@ -1,85 +1,60 @@
 // Create Checkout Session endpoint
 const Stripe = require("stripe");
+const { Payment, User } = require('../../models')
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 async function checkout(req, res) {
-  const { product, userId } = req.body;
-  console.log(product, userId);
+  const { product, user } = req.body;
+
+  // console.log(product, user.userId);
   try {
     // check for userId exist or not if not return
-    // if (!userId) {
-    //   return res.status(401).json({ error: "UnAuthorized" });
-    // }
+    if (!user) {
+      return res.status(401).json({ error: "UnAuthorized" });
+    }
     //check for subscription already if already then return
-    // const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
-    //   {
-    //     quantity: 1,
-    //     price_data: {
-    //       currency: "USD",
-    //       product_data: {
-    //         name: course.title,
-    //         description: course.description!,
-    //       },
-    //       unit_amount: Math.round(course.price! * 100),
-    //     }
-    //   }
-    // ];
+
+
     //check if already a stripe customer if not a stripe customer then create
-    // let stripeCustomer = await db.stripeCustomer.findUnique({
-    //   where: {
-    //     userId: user.id,
-    //   },
-    //   select: {
-    //     stripeCustomerId: true,
-    //   },
-    // });
 
-    // if (!stripeCustomer) {
-    //   const customer = await stripe.customers.create({
-    //     email: user.emailAddresses[0].emailAddress,
-    //   });
+    let payment = await Payment.findOne({ userId: user.userId });
 
-    //   stripeCustomer = await db.stripeCustomer.create({
-    //     data: {
-    //       userId: user.id,
-    //       stripeCustomerId: customer.id,
-    //     },
-    //   });
-    // }
-
-    //create session for checkout payment
-    // const session = await stripe.checkout.sessions.create({
-    //   customer: stripeCustomer.stripeCustomerId,
-    //   line_items,
-    //   mode: 'payment',
-    //   success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?success=1`,
-    //   cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?canceled=1`,
-    //   metadata: {
-    //     courseId: course.id,
-    //     userId: user.id,
-    //   }
-    // });
+    if (payment == null) {
+      const customer = await stripe.customers.create({
+        email: user.email,
+      });
+      payment = await Payment.insertOne({
+        userId: user.id,
+        stripeCustomerId: customer.id,
+        tierId: product.tierId,
+      },
+      );
+    }
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: "usd",
+            currency: "inr",
             product_data: {
-              name: product.name,
+              type: product.type,
+              description: product.description,
+              period: product.period,
+              tier: product.tier
+
             },
             unit_amount: product.price * 100,
           },
-          quantity: product.quantity,
         },
       ],
+      customer: stripeCustomer.stripeCustomerId,
       payment_method_types: ["card"],
       mode: "payment",
-      success_url: "http://localhost:3001/success", // Redirect to frontend success page
-      cancel_url: "http://localhost:3001/cancel", // Redirect to frontend cancel page
-      // metadata: {
-      //   userId: userId, // Include the user ID as metadata
-      // },
+      success_url: process.env.STRIPE_PAYMENT_SUCCESS_URL, // Redirect to frontend success page
+      cancel_url: process.env.STRIPE_PAYMENT_CANCEL_URL, // Redirect to frontend cancel page
+      metadata: {
+        userId: user.userId, // Include the user ID as metadata
+      },
     });
     res.json({ sessionId: session.id });
   } catch (error) {
--- schema/paymentSchema.js
+++ schema/paymentSchema.js
@@ -6,12 +6,13 @@ const paymentSchema = mongoose.Schema({
     required: true,
     ref: "users",
   },
-  paymentType: { type: String, required: true, enum: ["CARD", "UPI"] },
-  transactionId: { type: String, required: true },
+  paymentType: { type: String, required: true, enum: ["CARD", "UPI"], default: "CARD" },
+  transactionId: { type: String, required: false },
+  stripeCustomerId: { type: String, required: true },
   tierId: { type: mongoose.Schema.Types.ObjectId, ref: "tier", required: true },
-  expiredOn: { type: Date, required: true },
+  expiredOn: { type: Date, required: false },
   autorenewal: { type: Boolean, required: true, default: false },
-  renewalType: { type: String, enum: ["MONTHLY", "YEARLY"] },
+  renewalType: { type: String, enum: ["MONTHLY", "QUATERLY", "ANNUALLY"], default: "MONTHLY" },
 });
 
 module.exports = paymentSchema;
