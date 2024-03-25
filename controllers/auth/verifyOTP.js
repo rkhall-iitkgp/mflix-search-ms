3960e049c10a3fea3bec11f40146bc343a7b93e0
likhnic <likhith26090@gmail.com>
2024-03-26 00:56:01 +0530

    Changed login a bit

--- controllers/auth/verifyOTP.js
+++ controllers/auth/verifyOTP.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const { Account } = require("../../models");
+const { Account , User} = require("../../models");
 const { client } = require("../../redis");
 const jwt = require("jsonwebtoken");
 
@@ -8,8 +8,8 @@ const verifyOTP = async (req, res) => {
     const {type} = req.body;
 
     if(type==="change"){
-        if(!req.headers.authorization) return res.status(401).json({ message: "Error in Authentication", success: false});
-        const token = req.headers.authorization.split(" ")[1];
+        if(!req.cookies.accessToken) return res.status(401).json({ message: "Error in Authentication", success: false});
+        const token = req.cookies.accessToken;
         const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
         req.user = decoded;
     }
@@ -44,7 +44,31 @@ const verifyOTP = async (req, res) => {
         const { name, phone, dob } = req.body;
         const hash = await bcrypt.hash(newPassword, 10);
         const user = new Account({ name, email, password: hash, phone, dob });
+
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        };
+
+        const token = jwt.sign(payload, process.env.ACCESS_SECRET, {
+            expiresIn: process.env.ACCESS_EXPIRE_TIME,
+        });
+
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: process.env.DEPLOYMENT === "local" ? false : true,
+        });
+
+        const userProfile = new User({ name: user.name });
+        await userProfile.save();
+
+        user.userProfiles.push(userProfile);
         await user.save();
+
+        user.password = undefined;
+
+        return res.status(200).json({ message: "User registered successfully", success: true, account: user });
     }
 
     if(type==="forgot" || type==="change"){
--- middlewares/authMiddle.js
+++ middlewares/authMiddle.js
@@ -1,16 +1,87 @@
 const jwt = require("jsonwebtoken");
+const { ActiveLogin } = require("../models");
 
-const auth = (req, res, next) => {
+const refresh = async(refreshToken) => {
+
     try {
-        if(!req.headers.authorization) {
+        let payload = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+        let user = await Account.findOne({ email: payload.email }).exec();
+        if (!user) {
+            return {
+                success: false,
+                message: "User not found",
+            };
+        }
+        const newPayload = {
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        };
+        let token = jwt.sign(newPayload, process.env.ACCESS_SECRET, {
+            expiresIn: process.env.ACCESS_EXPIRE_TIME,
+        });
+        user = user.toObject();
+        user.password = undefined;
+        return {
+            success: true,
+            message: "Token refreshed",
+            "account":user,
+            token: token
+        };
+    }
+    catch (error) {
+        console.error(error);
+        return {
+            success: false,
+            message: "Error occured in refresh",
+        };
+    }
+}  
+
+const auth = async(req, res, next) => {
+    try {
+        const token = req.cookies.accessToken;
+        const refreshToken = req.cookies.refreshToken;
+
+        if(!refreshToken){
+            res.clearCookie('accessToken');
             return res.status(401).json({
                 success: false,
-                message: "Error in Authentication",
+                message: "No refresh token provided",
             });
         }
-        const token = req.headers.authorization.split(" ")[1];
+
+        if(!token){
+            const refreshResponse = await refresh(refreshToken);
+            if(!refreshResponse.success){
+                res.clearCookie('refreshToken');
+                return res.status(401).json({
+                    success: false,
+                    message: refreshResponse.message,
+                });
+            }
+
+            res.cookie("accessToken", refreshResponse.token, {
+                expires: new Date(Date.now() + 60 * 60 * 1000),
+                httpOnly: true,
+                secure: process.env.DEPLOYMENT === "local" ? false : true,
+            });
+        }
+
+
         const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
         req.user = decoded;
+        const activeLoginInstance = await ActiveLogin.findOne({ sessionId: refreshToken, account: req.user.id }).exec();
+
+        if(!activeLoginInstance){
+            res.clearCookie('accessToken');
+            res.clearCookie('refreshToken');
+            return res.status(401).json({
+                success: false,
+                message: "Active login session not found",
+            });
+        }
+
         next();
     } 
     catch (error) {
