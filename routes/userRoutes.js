b34167332a7b4d476c2167ad5085c2a918024d5f
likhnic <likhith26090@gmail.com>
2024-03-27 02:34:07 +0530

    Fixed routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { createUser, deleteUser } = require("../controllers/user");
 const { auth } = require("../middlewares");
-const {InitialFilter, CreateFilter, DeleteFilter, GetFilter,deleteFromFavourites,deleteSearchHistory,deleteFromWatchlist,addToWatchlist,saveSearch,saveWatchHistory,addToFavourites ,getWatchHistory,getFavourites,getWatchlist,getSearchHistory} = require("../controllers/user");
+const { CreateFilter, DeleteFilter, GetFilter,deleteFromFavourites,deleteSearchHistory,deleteFromWatchlist,addToWatchlist,saveSearch,saveWatchHistory,addToFavourites ,getWatchHistory,getFavourites,getWatchlist,getSearchHistory} = require("../controllers/user");
 router.get("/", (req, res) => {
   res.send("hello");
 });
-// router.get("/filter", InitialFilter);
 router.get("/filter", GetFilter);
 router.post("/filter", CreateFilter);
 router.delete("/filter", DeleteFilter);
