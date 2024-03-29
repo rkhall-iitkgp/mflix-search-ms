5444d35a4f25e85573cb2afd4121a18730fc1002
likhnic <likhith26090@gmail.com>
2024-03-29 19:20:22 +0530

    Fixed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         cookie: {
-            secure: false, 
+            secure: true, 
             sameSite: 'none'
         }
     }),
