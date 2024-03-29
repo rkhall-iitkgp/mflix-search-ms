2b617717ce83a4576c9bd5073ae3b54a3ec70aa1
likhnic <likhith26090@gmail.com>
2024-03-30 04:09:22 +0530

    Added inFav, in Watch

--- controllers/movie/movieById.js
+++ controllers/movie/movieById.js
@@ -1,11 +1,39 @@
-const { Movie } = require("../../models");
+const { Movie, User } = require("../../models");
+const jwt = require("jsonwebtoken");
 
-async function moviesbyid(req, res){
+async function moviesbyid(req, res) {
     try {
         const { id } = req.params;
-        const result = await Movie.findById(id).select(
+        let result = await Movie.findById(id).select(
             "-plot_embedding -fullplot_embedding -embedding",
-        );
+        ).exec();
+        if (req.body.userId) {
+            const decoded = jwt.verify(req.cookies.accessToken, process.env.ACCESS_SECRET);
+
+            if(!decoded) {
+                return res.status(401).json({
+                    status: false,
+                    message: "Unauthorized",
+                });
+            }
+
+            const user = await User.findById(req.body.userId).exec();
+            console.log(user);
+            result = result.toObject();
+            result.inWatchlist = false;
+            user.watchList.forEach((movie) => {
+                console.log(movie, id);
+                if (movie.movie.toString() === id) {
+                    result.inWatchlist = true;
+                }
+            });
+            result.inFavorites = false;
+            user.favoriteMovies.forEach((movie) => {
+                if (movie.movie.toString() === id) {
+                    result.inFavorites = true;
+                }
+            })
+        }
         res.status(200).json({
             status: true,
             result,
@@ -17,4 +45,4 @@ async function moviesbyid(req, res){
         });
     }
 };
-module.exports=moviesbyid
+module.exports = moviesbyid
