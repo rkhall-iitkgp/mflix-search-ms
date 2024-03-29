c21352227db780781fb5a03535faa0bfd2d7044a
likhnic <likhith26090@gmail.com>
2024-03-29 18:57:49 +0530

    Fixed

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -129,6 +129,7 @@ const login = async (req, res) => {
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true,
             // secure: process.env.DEPLOYMENT === "local" ? false : true,
+            sameSite: "none",
         });
 
         res.cookie("accessToken", token, {
