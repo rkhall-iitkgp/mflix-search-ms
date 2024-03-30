37b29815b4595604d06f493d2e881b4b1ec44da2
Ritik Raj <ritikraj1369@gmail.com>
2024-03-30 22:44:28 +0530

    Refactor movieLinkById controller and movieRoutes

--- controllers/movie/movieLinkById.js
+++ controllers/movie/movieLinkById.js
@@ -2,46 +2,78 @@ const { Movie, Account } = require("../../models");
 
 module.exports = async (req, res) => {
     try {
-        const { user } = req;
-        let account = await Account.findById(req.user.id)
+        const result = await Movie.findById(id)
+            .select({
+                videoSrc: 1,
+            })
+            .exec();
+        const movie = result.toObject();
+        let token = req.cookies.accessToken;
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            if (movie.tier.toLowerCase() === "free")
+                return res.status(200).json({
+                    success: true,
+                    result: result,
+                });
+        }
+
+        if (!token) {
+            const refreshResponse = await refresh(refreshToken);
+            if (!refreshResponse.success) {
+                res.clearCookie("refreshToken");
+                return res.status(401).json({
+                    success: false,
+                    message: refreshResponse.message,
+                });
+            }
+
+            res.cookie("accessToken", refreshResponse.token, {
+                expires: new Date(Date.now() + 60 * 60 * 1000),
+                httpOnly: true,
+                // secure: process.env.DEPLOYMENT === "local" ? false : true,
+            });
+
+            token = refreshResponse.token;
+        }
+
+        const user = jwt.verify(token, process.env.ACCESS_SECRET);
+
+        let account = await Account.findById(user.id)
             .populate({
                 path: "subscriptionTier",
-                populate: { path: "tier", model: "tiers" }
+                populate: { path: "tier", model: "tiers" },
             })
-            .exec()
+            .exec();
 
         account = account.toObject();
 
         const { id } = req.params;
-        const result = await Movie.findById(id)
-            .select({
-                videoSrc: 1,
-            }).exec();
-        const movie = result.toObject();
-
 
         if (account.subscriptionTier.tier.name === "Premium") {
             res.status(200).json({
                 status: true,
-                result
+                result,
             });
         } else {
             if (movie.tier.toLowerCase() === "free") {
                 res.status(200).json({
                     status: true,
-                    result
+                    result,
                 });
             } else {
                 res.status(200).json({
-                    status: true,
-                    result: {}
+                    status: false,
+                    message: "Upgrade you tier to enjoy this movie",
+                    result: null,
                 });
             }
         }
     } catch (error) {
         res.status(500).json({
             status: false,
-            message: "Error: " + error.message
+            message: "Error: " + error.message,
         });
     }
 };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { GetAllMovies, GetMovieById ,GetByAwards, GetVideoLink} = require("../controllers/movie");
-const {auth} = require("../middlewares");
+const {
+    GetAllMovies,
+    GetMovieById,
+    GetByAwards,
+    GetVideoLink,
+} = require("../controllers/movie");
+const { auth } = require("../middlewares");
 const router = express.Router();
 
 router.get("/", GetAllMovies);
 router.get("/awards", GetByAwards);
-router.get("/link/:id",auth, GetVideoLink)
+router.get("/link/:id", GetVideoLink);
 router.post("/:id", GetMovieById);
 
 module.exports = router;
