4883829d2c89e3fdc2566264b205b2fbd5cd5f94
likhnic <likhith26090@gmail.com>
2024-03-30 20:49:44 +0530

    Added put request for account

--- controllers/user/details.js
+++ controllers/user/details.js
@@ -37,4 +37,40 @@ const getUserDetails = async (req, res) => {
     }
 }
 
-module.exports = {getUserDetails}
+
+const updateUserDetails = async (req, res) => {
+
+    try{
+
+        const {name, phone} = req.body;
+        const {email} = req.user;
+
+        const user = await Account.findOne({email}).exec();
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        if(name) user.name = name;
+        if(phone) user.phone = phone;
+
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "User details updated successfully",
+            user: user
+        })
+    }
+    catch(error){
+        console.error("Error: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error: " + error.message
+        })
+    }       
+}
+
+module.exports = {getUserDetails, updateUserDetails}
