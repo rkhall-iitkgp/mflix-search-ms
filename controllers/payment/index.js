bd49fc7d93a9680f624f4ffc75bf532d67fcafce
likhnic <likhith26090@gmail.com>
2024-03-30 17:08:04 +0530

    Added get route in payment

--- controllers/payment/index.js
+++ controllers/payment/index.js
@@ -1,6 +1,8 @@
 const checkout = require("./checkout");
 const webhook = require("./webhook");
+const {getPaymentDetails} = require("./details");
 module.exports = {
     checkout,
     webhook,
+    getPaymentDetails
 };
